b70486418a35e9d4a292364b3e921b46a9d7c28f
Omar Mohsen <87346785+OmarMohsen9@users.noreply.github.com>
2025-08-11 15:00:38 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+##config.py
+
+baseurl = 'http://live.techpanda.org/'
+mobile_page_url = 'http://live.techpanda.org/index.php/mobile.html'
+Expected_HomePage_title = 'THIS IS DEMO SITE FOR   '
+Expected_mobilePage_title = 'MOBILE'
+Implicit_wait = 10 # in seconds
+
+
+###########
